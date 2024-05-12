880bb4b26671e414b2e57f82aef143460f2b268b
KonstantinovIKM723a <alex7wrt@gmail.com>
2024-05-13 01:26:07 +0300

    Update

--- lab_18-variant_11/lab_18-variant_11.cpp
+++ lab_18-variant_11/lab_18-variant_11.cpp
@@ -1,20 +1,68 @@
-// lab_18-variant_11.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include "windows.h"
+using namespace std;
 
-#include <iostream>
+class SingleRandomMatrixOfFive {
+    int data[5] = {};
+public:
+    SingleRandomMatrixOfFive() {
+        for (int i = 0; i < 5; i++) data[i] = rand() % 10;
+    };
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    int getValueByIndex(int id) {
+        return data[id];
+    }
+
+    void showData() {
+        for (int i = 0; i < 5; i++) cout << data[i] << " ";
+        cout << endl;
+    }
+
+    SingleRandomMatrixOfFive operator+(SingleRandomMatrixOfFive v2) {
+        SingleRandomMatrixOfFive t;
+        for (int i = 0; i < 5; i++) t.data[i] = data[i] + v2.data[i];
+        return t;
+    }
+    SingleRandomMatrixOfFive operator-(SingleRandomMatrixOfFive v2) {
+        SingleRandomMatrixOfFive t;
+        for (int i = 0; i < 5; i++) t.data[i] = data[i] - v2.data[i];
+        return t;
+    }
+
+    SingleRandomMatrixOfFive operator*(SingleRandomMatrixOfFive v2) {
+        SingleRandomMatrixOfFive t;
+        for (int i = 0; i < 5; i++) t.data[i] = data[i] * v2.data[i];
+        return t;
+    };
+};
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    srand(time(NULL));
+
+    cout << "Перший масив (a): " << endl;
+    auto a = SingleRandomMatrixOfFive();
+    a.showData();
+
+    cout << "Другий масив (b): " << endl;
+    auto b = SingleRandomMatrixOfFive();
+    b.showData();
+
+    cout << "Сума масивів (a + b): " << endl;
+    auto c = a + b;
+    c.showData();
+
+    cout << "Різниця масивів (a - b): " << endl;
+    c = a - b;
+    c.showData();
+
+    cout << "Різниця масивів (b - a): " << endl;
+    c = b - a;
+    c.showData();
+
+    cout << "Добуток масивів (a * b): " << endl;
+    c = a * b;
+    c.showData();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
